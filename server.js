83b7e82372ad22619cc0268689aeef3aaedc679d
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-04-19 11:44:20 -0400

    correções de bug

--- server.js
+++ server.js
@@ -1,117 +1,101 @@
-const http = require('http')
+const http = require('http');
 const path = require('path');
-const  fs = require('fs'); 
-const fsPromises = require('fs').promises
-const logEvents = require( './LogEvents');
-const EventEmitter = require( 'events');
-const { error } = require('console');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
 
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter { };
 // initialize object 
+const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+const PORT = process.env.PORT || 3500;
 
-class MyEmitter  extends  EventEmitter {}
-
-const myEmitter = new MyEmitter();
-
-const PORT = process.env.PORT ||3500 ;
-
-const serverFile = async ( filePath, contentType, response) => {
-
-    try{
-      const rawData = await fsPromises.readFile(filePath,
-         !contentType.incluides('image') ? ' utf8 ' : ''
-        )
-      const data = contentType == ' application/json'
-          ? JSON.parse(rawData) : rawData
-      response.writeHead( 200 , { 'Content-Type':  contentType})
-      response.end(
-        contentType === 'application/json' ? JSON.stringify(data): data
-        
-        )
-    }catch ( err) {
-
-        console.log(err)
-        response.statusCode = 500
-        response.end();  
-
-
+const serveFile = async (filePath, contentType, response) => {
+    try {
+        const rawData = await fsPromises.readFile(
+            filePath,
+            !contentType.includes('image') ? 'utf8' : ''
+        );
+        const data = contentType === 'application/json'
+            ? JSON.parse(rawData) : rawData;
+        response.writeHead(
+            filePath.includes('404.html') ? 404 : 200,
+            { 'Content-Type': contentType }
+        );
+        response.end(
+            contentType === 'application/json' ? JSON.stringify(data) : data
+        );
+    } catch (err) {
+        console.log(err);
+        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
+        response.statusCode = 500;
+        response.end();
     }
-
-
-
-
-
 }
 
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
 
-const server = http.createServer(( req, res ) => {
-    console.log(req.url, req.method)
+    const extension = path.extname(req.url);
 
-    const extension = path.extname( req.url)
+    let contentType;
 
-    let  contentType;
-
-    switch(extension){
+    switch (extension) {
         case '.css':
-            contentType  = 'text/css';
-        break;
+            contentType = 'text/css';
+            break;
         case '.js':
-             contentType = ' text/javascript';
-        break;
+            contentType = 'text/javascript';
+            break;
         case '.json':
-            contentType =' application/json';
+            contentType = 'application/json';
             break;
         case '.jpg':
-            contentType = 'image/jpeg'
+            contentType = 'image/jpeg';
             break;
-        case '.png ':
-            contentType = 'image/png'
+        case '.png':
+            contentType = 'image/png';
             break;
         case '.txt':
-            contentType = ' text/plain'
+            contentType = 'text/plain';
             break;
         default:
-            contentType = 'text.html '
-            break
-    
+            contentType = 'text/html';
     }
-    let filePath = 
-        contentType ==='text/html' && req.url ==='/'
-            ? path.join(__dirname, 'Views', ' index.html')
-            :contentType === 'text/html' && req.url.slice(-1) ==='/'
-                 ?  path.join(__dirname, 'Views', ' index.html')
-                 :  contentType === 'text/html'
-                        ? path.join(__dirname, 'Views',  req.url)
-                        : path.join(__dirname,   req.url)
 
-      if (!extension && req.url.slice( -1 ) !== '/') filePath += '.html'
+    let filePath =
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
 
-      const  fileExists = fs.existsSync ( filePath)
-      if( fileExists) {
+    // makes .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
 
-        serverFile(filePath,contentType,res)
+    const fileExists = fs.existsSync(filePath);
 
-      }else {
-        switch( path.parse(filePath).base){
+    if (fileExists) {
+        serveFile(filePath, contentType, res);
+    } else {
+        switch (path.parse(filePath).base) {
             case 'old-page.html':
-                res. writeHead(301,{'Location': '/Views/new-page.html' })
+                res.writeHead(301, { 'Location': '/new-page.html' });
                 res.end();
-                break
-            case ' www-page.html':
-                res.writeHead (301,{'Location': '/Views/index.html'})
-                res.end()
-                break
+                break;
+            case 'www-page.html':
+                res.writeHead(301, { 'Location': '/' });
+                res.end();
+                break;
             default:
-                serverFile(path.join(__dirname, 'Views', '404.html'), 'text/html', res)
-
-
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
         }
-      }
-      
-      
-})
-
-     
-
-server.listen(PORT, () => console.log(`Server  is running in ${PORT}`));
+    }
+});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
