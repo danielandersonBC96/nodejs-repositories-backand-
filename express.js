66aff0b36df71e0235067588517ee3535849a771
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-04-19 15:54:57 -0400

    finish

--- express.js
+++ express.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const app = express( );
 const path = require('path')
-const PORT= process.env.PORT || 35000;
+
+const PORT = process.env.PORT || 3500;
 
 
-app.get('/' ,( req , res ) => {
-    res.send('hello word ')
+app.get('^/$|/index(.html)?',( req , res ) => {
+
+    ///res.sendFile('./Views/index.html', { root: __dirname})
+
+    res.sendFile(path.join(__dirname, 'Views', 'index.html'))
+
+})
+
+app.get('/new-page(.html)?',( req , res ) => {
+
+    ///res.sendFile('./Views/index.html', { root: __dirname})
+
+    res.sendFile(path.join(__dirname, 'Views', 'new-page.html'))
 
 
 })
 
-app.listen(PORT, () => console.log (` Server is running on port ${PORT}`))
+app.get('/old-page(.html)?',( req , res ) => {
+
+    ///res.sendFile('./Views/index.html', { root: __dirname})
+   //  res.sendFile(path.join(__dirname, 'Views', 'new-page.html'))
+
+    res.redirect(301 , '/new-page.html')
+
+})
+
+app.get ('/*', (req, res ) => {
+
+    res.status(404).sendFile(path.join( __dirname , 'Views ', '404.html' ))
+
+
+
+})
+
+app.get('/hello(.html)?' ,  ( req , res , next)  => {
+     console.log('attempetd to load hello.hrml')
+     next()
+} , ( req, res) => {
+
+    res.send('hello world ')
+})
+
+
+app.listen(PORT, () => console.log (` express  is running on port ${PORT}`))
